122ca5652ad4194eb99d54a3bd4990395f758162
yuta-muramoto <s1513114@u.tsukuba.ac.jp>
2015-10-18 08:03:48 +0900

    modify new.html.erb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,27 @@ class QuestionsController < ApplicationController
     def new
         # @quesion = Quesions.new
     end
+
+		def create
+		    @question = Question.new(article_params)
+
+				if @question.save
+					redirect_to @question
+				else
+					render 'new'
+				end
+		end
+
+		def show
+		    @question = Question.find(params[:id])
+		end
+
+		def index
+		    @question = Question.all
+		end
+
+		private
+		    def article_params
+				    params.require(:question).permit(:title);
+				end
 end
