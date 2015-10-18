82f19806c3e04cf29b61a30d613fdda35a571e2f
kuwako <masaki.kuwako@leverages.jp>
2015-10-18 10:00:50 +0900

    topのview側作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   resources :details
   resources :tops
+  root 'tops#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
