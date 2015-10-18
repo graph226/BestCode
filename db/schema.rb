66edfad682dea8ade8062480cb77b7dab4d414ce
kuwako <masaki.kuwako@leverages.jp>
2015-10-18 10:41:03 +0900

    delete migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018011539) do
+ActiveRecord::Schema.define(version: 20151018003529) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20151018011539) do
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
